c2c0699f144b8fd83a037e630a00184c975d960d
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-04-12 14:52:23 +0200

    [add] file form

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+<body>
+  
+<div class="container">
+
+
+<div class="input-group mb-3">
+  <span class="input-group-text">With textarea</span>
+  <textarea class="form-control" aria-label="With textarea"></textarea>
+</div>
+<div class=" ">
+  <label for="basic-url" class="form-label">Inserisci la parola da censurare</label>
+  <div><input type="text" class="form-control" placeholder="Username" aria-label="Username">
+  </div>
+  
+</div>
+
+</div>  
+</body>
+</html>
